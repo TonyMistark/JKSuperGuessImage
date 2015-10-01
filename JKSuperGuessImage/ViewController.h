487dfa95ae67ea11be04baeee725c0a420182c87
TonyMistark <tony_mistark@163.com>
2015-10-01 12:34:37 +0800

    Initial Commit

---
+++ JKSuperGuessImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKSuperGuessImage
+//
+//  Created by 弥超 on 15/10/1.
+//  Copyright © 2015年 弥超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JKSuperGuessImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JKSuperGuessImage
+//
+//  Created by 弥超 on 15/10/1.
+//  Copyright © 2015年 弥超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
